fba42f186aa596e589cc7caa57980875490899f2
rachel490 <ccori12@naver.com>
2023-10-31 01:02:37 +0900

    [#1.1 your first component] challenge 2

--- Describing_the_UI/Your_First_Component/src/App.js
+++ Describing_the_UI/Your_First_Component/src/App.js
@@ -1,9 +1,11 @@
 import Profile1 from './components/Challenge1'
+import Profile2 from './components/Challenge2'
 
 export default function App() {
   return (
     <div>
       <Profile1 />
+      <Profile2 />
     </div>
   )
 }
