0d694319fc863e2968b411bac12db300807aa962
rachel490 <ccori12@naver.com>
2023-10-31 01:03:59 +0900

    [#1.1 your first component] challenge 3

--- Describing_the_UI/Your_First_Component/src/App.js
+++ Describing_the_UI/Your_First_Component/src/App.js
@@ -1,11 +1,13 @@
 import Profile1 from './components/Challenge1'
 import Profile2 from './components/Challenge2'
+import Gallery from './components/Challenge3'
 
 export default function App() {
   return (
     <div>
       <Profile1 />
       <Profile2 />
+      <Gallery />
     </div>
   )
 }
