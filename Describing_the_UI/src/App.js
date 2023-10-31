2544f246b362a0cfef40755c27e8f5f91157eff8
rachel490 <ccori12@naver.com>
2023-10-31 16:33:51 +0900

    change directory for #1.1

---
+++ Describing_the_UI/src/App.js
@@ -0,0 +1,15 @@
+import Profile1 from './components/1.your_first_component/Challenge1'
+import Profile2 from './components/1.your_first_component/Challenge2'
+import Gallery from './components/1.your_first_component/Challenge3'
+import Congratulations from './components/1.your_first_component/Challenge4'
+
+export default function App() {
+  return (
+    <div>
+      <Profile1 />
+      <Profile2 />
+      <Gallery />
+      <Congratulations />
+    </div>
+  )
+}
