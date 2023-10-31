71635fe738449313f5fde72b2b4f8502c91e1bd0
rachel490 <ccori12@naver.com>
2023-10-31 17:10:21 +0900

    [#1.4 javascript in jsx] challenge 1

--- Describing_the_UI/src/App.js
+++ Describing_the_UI/src/App.js
@@ -1,13 +1,15 @@
 import YourFirstComponent from './components/1.your_first_component'
 import ImportAndExportingComponents from './components/2.importing_and_exporting_components'
 import WritingMarkupWithJSX from './components/3.writing_markup_with_jsx'
+import JavascriptInJSXWithCurlyBraces from './components/4.javascript_in_jsx'
 
 export default function App() {
   return (
     <div>
       {/* <YourFirstComponent /> */}
       {/* <ImportAndExportingComponents /> */}
-      <WritingMarkupWithJSX />
+      {/* <WritingMarkupWithJSX /> */}
+      <JavascriptInJSXWithCurlyBraces />
     </div>
   )
 }
---
+++ Describing_the_UI/src/components/4.javascript_in_jsx/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TodoList from './TodoList'
+
+export default function JavascriptInJSXWithCurlyBraces() {
+  return (
+    <div>
+      <h2>[1.4] Javascript In JSX With Curly Braces</h2>
+      <TodoList />
+    </div>
+  )
+}
