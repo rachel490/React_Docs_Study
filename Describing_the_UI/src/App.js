a3c235ce8aba1a14e857ffdcbab568f16f9cd118
rachel490 <ccori12@naver.com>
2023-10-31 17:01:01 +0900

    [#1.3 writing markup with jsx] challenge 1

--- Describing_the_UI/src/App.js
+++ Describing_the_UI/src/App.js
@@ -1,11 +1,13 @@
 import YourFirstComponent from './components/1.your_first_component'
 import ImportAndExportingComponents from './components/2.importing_and_exporting_components'
+import WritingMarkupWithJSX from './components/3.writing_markup_with_jsx'
 
 export default function App() {
   return (
     <div>
       {/* <YourFirstComponent /> */}
-      <ImportAndExportingComponents />
+      {/* <ImportAndExportingComponents /> */}
+      <WritingMarkupWithJSX />
     </div>
   )
 }
---
+++ Describing_the_UI/src/components/3.writing_markup_with_jsx/index.jsx
@@ -0,0 +1,11 @@
+import Bio from './Bio'
+import './styles.css'
+
+export default function WritingMarkupWithJSX() {
+  return (
+    <div>
+      <h2>[1.3] Writing Markup With JSX</h2>
+      <Bio />
+    </div>
+  )
+}
