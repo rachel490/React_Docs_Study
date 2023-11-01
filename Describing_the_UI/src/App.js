6ba0c55dd6f9be736753289dccbe8a672904b9a8
rachel490 <ccori12@naver.com>
2023-11-01 18:33:22 +0900

    [#1.6 conditional rendering] challenge 1

--- Describing_the_UI/src/App.js
+++ Describing_the_UI/src/App.js
@@ -3,6 +3,7 @@ import ImportAndExportingComponents from './components/2.importing_and_exporting
 import WritingMarkupWithJSX from './components/3.writing_markup_with_jsx'
 import JavascriptInJSXWithCurlyBraces from './components/4.javascript_in_jsx'
 import PassingPropsToAComponent from './components/5.passing_props'
+import ConditionalRendering from './components/6.conditional_rendering'
 
 export default function App() {
   return (
@@ -11,7 +12,8 @@ export default function App() {
       {/* <ImportAndExportingComponents /> */}
       {/* <WritingMarkupWithJSX /> */}
       {/* <JavascriptInJSXWithCurlyBraces /> */}
-      <PassingPropsToAComponent />
+      {/* <PassingPropsToAComponent /> */}
+      <ConditionalRendering />
     </div>
   )
 }
---
+++ Describing_the_UI/src/components/6.conditional_rendering/index.jsx
@@ -0,0 +1,10 @@
+import PackingList from './PackingList'
+
+export default function ConditionalRendering() {
+  return (
+    <div>
+      <h2>Conditional Rendering</h2>
+      <PackingList />
+    </div>
+  )
+}
