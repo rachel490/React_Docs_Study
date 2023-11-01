c5525ac131d0859cb2290b2d6ea5c1375ca0cd8f
rachel490 <ccori12@naver.com>
2023-11-01 19:38:06 +0900

    [#1.7 rendering lists] challenge 3

--- Describing_the_UI/src/components/7.rendering_lists/RecipeList.jsx
+++ Describing_the_UI/src/components/7.rendering_lists/RecipeList.jsx
@@ -1,19 +1,26 @@
 import { recipes } from './data.js'
 
+export function Recipe({ recipe }) {
+  return (
+    <li key={recipe.id}>
+      <h2>{recipe.name}</h2>
+      <ul>
+        {recipe.ingredients.map(ingredient => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+    </li>
+  )
+}
+
+// NOTE: key in the component => bc the component should be differentiated
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       <ul>
         {recipes.map(recipe => (
-          <li key={recipe.id}>
-            <h2>{recipe.name}</h2>
-            <ul>
-              {recipe.ingredients.map(ingredient => (
-                <li key={ingredient}>{ingredient}</li>
-              ))}
-            </ul>
-          </li>
+          <Recipe key={recipe.id} recipe={recipe} />
         ))}
       </ul>
     </div>
